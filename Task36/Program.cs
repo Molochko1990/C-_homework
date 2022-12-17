55ebd9e745233e6724c7e159ea724d84dd08f2e3
Molochko1990 <Es01@bk.ru>
2022-12-17 15:05:42 +0500

    Added task36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random rand = new Random();
+    int count = rand.Next(2, 10);
+    int[] arr = new int[count];
+    for (int i = 0; i < arr.Length; i++)
+        arr[i] = rand.Next(100, 999);
+        Console.WriteLine("======= Массив =======");
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write(arr[i] + " "); 
+            
+        }
+        Console.WriteLine();
+        Console.WriteLine("======================");
+        int result = 0;
+        for (int i = 0; i < count; i++)
+        {
+            if (i % 2 != 0) result = arr[i] + result;
+        }
+        Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях = {result}");
