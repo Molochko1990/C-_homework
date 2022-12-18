45af374193de9fcef57b4b2e3ed5d5f4427222f0
Molochko1990 <Es01@bk.ru>
2022-12-18 14:39:30 +0500

    hw done

---
+++ Task41/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Программа №41");
+Console.WriteLine("=============");
+Console.WriteLine();
+Console.WriteLine("Введите несколько чисел");
+string inp = Convert.ToString(Console.ReadLine());
+string[] array = inp.Split(new char[]{' ', ','}, StringSplitOptions.RemoveEmptyEntries);
+int[] arr = array.Select(Int32.Parse).ToArray();
+int amount = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (arr[i] > 0) amount++;
+}
+Console.WriteLine(amount);
---
+++ Task43/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("=============");
+Console.WriteLine("Программа №43");
+Console.WriteLine("=============");
+Console.WriteLine();
+// Input data
+Console.WriteLine("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+// Programm
+double x = (b1 - b2)/(k2 - k1);
+double y = (k2 * b1 - k1 * b2)/(k2 - k1);
+if (k1 == k2) Console.WriteLine("Прямые не пересекаются");
+else Console.WriteLine($"Прямые пересекаются в ({x};{y})");
