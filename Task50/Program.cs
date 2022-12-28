9e23847c22d08687ae878228d6fda6d8316c5bc0
Molochko1990 <Es01@bk.ru>
2022-12-28 14:59:05 +0500

    Added task50

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("=============");
+Console.WriteLine("Программа №50");
+Console.WriteLine("=============");
+Console.WriteLine(); 
+
+int [] array = {2, 5, 9, 21, 66}; 
+Console.WriteLine("Введите индекс элемента в массиве");
+int i = Convert.ToInt32(Console.ReadLine());
+try
+{
+    if (Convert.ToBoolean(array[i])) Console.WriteLine(array[i]);
+}
+catch (System.IndexOutOfRangeException)
+{
+    Console.WriteLine("Такого элемента нет");
+}
+
