59c6aaedc89a92abc8f4123d7569d2d5fadb4692
Molochko1990 <Es01@bk.ru>
2022-12-28 14:07:15 +0500

    Added Task47

---
+++ Task47/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("=============");
+Console.WriteLine("Программа №47");
+Console.WriteLine("=============");
+Console.WriteLine();
+
+Console.WriteLine("Введите два числа");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+double [] array = new double[m * n];
+Random rand = new Random();
+Console.WriteLine("======");
+Console.WriteLine("Массив:");
+for (int i = 0; i < array.Length ; i++) 
+{
+    array[i] = rand.Next(0, 100) + rand.NextDouble();
+    Console.Write("| " + Convert.ToString(Math.Round(array[i], 2)) + " | ");
+}
